a545549ebd47e6cead8666c336d7b9dcb3a84d98
chandra dubey <chandra.dubey@squareboat.com>
2023-04-14 15:56:12 +0530

    ADD: Concurrecny Function

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import pLimit from "p-limit";
+import pLimit from "./rateLimit.js";
 import axios from "axios";
 import https from "https";
 
@@ -21,7 +21,18 @@ const sampleData = {
 // const limit = pLimit(3);
 
 // const requests = endpoints.map((account) => limit(() => axios.get(account)));
+function allSum(arr, data) {
+  // console.log("🚀 ~ file: app.js:25 ~ allSum ~ data:", data, arr);
+  let currSum = data.data.num.reduce(findSum, arr[0]);
+  let currlen = data.data.num.length + arr[1];
 
+  // let sum = arr[0] + currSum;
+  return [currSum, currlen];
+}
+function findSum(sum, data) {
+  sum += data;
+  return sum;
+}
 async function fun(endPoints, numOfConcurency) {
   try {
     const limit = pLimit(numOfConcurency);
@@ -33,14 +44,14 @@ async function fun(endPoints, numOfConcurency) {
     let avg = 0;
     let sum = 0;
     let numOfele = 0;
-
-    for (let data of responses) {
-      numOfele += data.data.num.length;
-      for (let it of data.data.num) {
-        sum += it;
-      }
-    }
-    avg = sum / numOfele;
+    const result = responses.reduce(allSum, [0, 0]);
+    // for (let data of responses) {
+    //   numOfele += data.data.num.length;
+    //   for (let it of data.data.num) {
+    //     sum += it;
+    //   }
+    // }
+    avg = result[0] / result[1];
 
     return avg;
   } catch (err) {
